b9e04ca62e2a7c56fc9aa09010430e767adab45e
Dereck Basilio <dereck@docufy.com>
2013-07-01 11:48:08 -0500

    Initial Commit

---
+++ MovieLibrary/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MovieLibrary.Models;
+
+namespace MovieLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext db = new MovieContext();
+        //
+        // GET: /Home/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(Movie movieToCreate)
+        {
+            try
+            {
+                db.Movies.Add(movieToCreate);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie != null)
+            {
+                db.Movies.Remove(movie);
+                db.SaveChanges();
+            }
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+    }
+}
---
+++ MovieLibrary/Models/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieLibrary.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext()
+            : base("MoviesDBEntities")
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+
+    public class Movie
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
